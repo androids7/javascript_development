0957d3887206ca8eabede50dc1987728d62d9b75
androids7 <1559215761@qq.com>
2017-12-09 09:48:33 +0800

    不必要

--- app/src/main/java/com/jide/Encode.java
+++ app/src/main/java/com/jide/Encode.java
@@ -2,20 +2,6 @@ package com.jide;
 
 public class Encode
 {
-	public static String encode(String str)
-	{
-		String key="10086";
-		char chs[]=str.toCharArray();
-		int ch[]=new int[chs.length];
-		for(int i=0;i<chs.length;i++)
-		{
-			ch[i]=(chs[i]<<2)^20;
-			chs[i]=(char)ch[i];
-		}
-		
-		return new String(chs);
-	}
-	
 	
 	public static String decode(String str)
 	{
